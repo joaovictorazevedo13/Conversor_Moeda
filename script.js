6ea1447ed52ec61e75153f68b529b6b9a1e230cc
João Victor Azevedo <109423515+joaovictorazevedo13@users.noreply.github.com>
2022-11-17 12:16:31 -0300

    att

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-var myHeaders = new Headers();
+function converter() {
+  let moeda_1 = window.document.querySelector('input#moeda1')
+  let type_moeda1 = window.document.querySelector('select#type_moeda1')
+  let type_moeda2 = window.document.querySelector('select#type_moeda2')
+  
+
+  var myHeaders = new Headers();
 myHeaders.append("apikey", "19y08ONRz6oGvbyurX7XkPUaCRXNmzw7");
 
 var requestOptions = {
@@ -7,7 +13,8 @@ var requestOptions = {
   headers: myHeaders
 };
 
-fetch("https://api.apilayer.com/fixer/convert?to=to&from=from&amount=amount", requestOptions)
+fetch(`https://api.apilayer.com/fixer/convert?to=${moeda_1}&from=from&amount=amount`, requestOptions)
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
+}
