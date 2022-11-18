4d508125c4e0b89088c96db7f7d5ec926a1004dd
João Victor Azevedo <109423515+joaovictorazevedo13@users.noreply.github.com>
2022-11-18 14:26:34 -0300

    att

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 function converter() {
-  let moeda_1 = window.document.querySelector('input#moeda1')
+  let valor = window.document.querySelector('input#moeda1')
   let type_moeda1 = window.document.querySelector('select#type_moeda1')
   let type_moeda2 = window.document.querySelector('select#type_moeda2')
-  
+  let result1 = window.document.querySelector('p#Resultado')
 
   var myHeaders = new Headers();
-myHeaders.append("apikey", "19y08ONRz6oGvbyurX7XkPUaCRXNmzw7");
+  myHeaders.append("apikey", "19y08ONRz6oGvbyurX7XkPUaCRXNmzw7");
 
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-  headers: myHeaders
-};
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
 
-fetch(`https://api.apilayer.com/fixer/convert?to=${moeda_1}&from=from&amount=amount`, requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-}
+  fetch(`https://api.apilayer.com/fixer/convert?to=${type_moeda1}&from=${type_moeda2}&amount=${valor}`, requestOptions)
+    .then(response => response.text())
+    .then(result => result1.innerHTML + result)
+    .catch(error => console.log('error', error));
+  result1.innerHTML + response
+  }
